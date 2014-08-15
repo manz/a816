1f0b09b7fab49286062f9bb5b5add3aa9a9b1e26
Emmanuel Peralta <manz@ringum.net>
2014-08-15 14:54:37 +0200

    Split blocks bigger than 0xFFFF in smaller blocks.

--- a816/writers.py
+++ a816/writers.py
@@ -13,8 +13,14 @@ class IPSWriter(object):
         self.file.write(struct.pack('>H', len(block)))
 
     def write_block(self, block, block_address):
-        self.write_block_header(block, block_address)
-        self.file.write(block)
+        k = 0
+        while block[k:]:
+            slice_size = min(0xFFFF, len(block) - k)
+            block_slice = block[k:k+slice_size]
+
+            self.write_block_header(block_slice, block_address)
+            self.file.write(block_slice)
+            k += slice_size
 
     def end(self):
         self.file.write(b'EOF')
