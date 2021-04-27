0469d16a3835578c45c193400f864eea45e18d0b
Emmanuel Peralta <manz@ringum.net>
2021-04-27 21:43:17 +0200

    Updates Table implementation to be more subclassable.

--- script/__init__.py
+++ script/__init__.py
@@ -15,24 +15,43 @@ class Table(object):
     @property
     def items(self):
         return self.lookup.items()
-
+    
+    
     def include(self, path):
         with open(path, 'rt', encoding='utf-8') as f:
             for line in f.readlines():
-                matches = self.table_line_regex.match(line)
-                if matches:
-                    byte = list(map(lambda b: int(''.join(b), 16), zip(*[iter(matches.group('byte'))] * 2)))
-                    text = matches.group('text')
-                    text = text.replace('\\n', '\n')
-                    self.lookup[text] = bytes(byte)
-
-                    if matches.group('ignore'):
-                        self.inverted_lookup[bytes(byte)] = (text, int(matches.group('ignore')))
-                    else:
-                        self.inverted_lookup[bytes(byte)] = text
+                self.parse_table_line(line)
 
             self.max_bytes_length = len(max(self.lookup.values(), key=len))
             self.max_text_length = len(max(self.lookup.keys(), key=len))
+
+    def parse_table_line(self, line) -> bool:
+        matches = self.table_line_regex.match(line)
+        if matches:
+            bytes_iterator = iter(matches.group('byte'))
+            byte = self.transform_byte_matches_to_int(bytes_iterator)
+            text = matches.group('text')
+            text = text.replace('\\n', '\n')
+            self.add_lookup(text, byte)
+
+            if matches.group('ignore'):
+                self.add_inverted_lookup(byte, text, int(matches.group('ignore')))
+            else:
+                self.add_inverted_lookup(byte, text)
+        return matches is not None
+
+    def transform_byte_matches_to_int(self, bytes_iterator):
+        return list(map(lambda b: int(''.join(b), 16), zip(*[bytes_iterator] * 2)))
+
+    def add_inverted_lookup(self, byte: bytes, text: str, ignore: int =None):
+        if ignore is not None:
+            self.inverted_lookup[byte] = (text, ignore)
+        else:
+            self.inverted_lookup[bytes(byte)] = text
+
+    def add_lookup(self, text, byte):
+
+        self.lookup[text] = bytes(byte)
 
     def to_bytes(self, text):
         binary_text = []
@@ -44,7 +63,7 @@ class Table(object):
 
             if matches:
                 binary_text += bytes([int(matches.group('byte'), 16)])
-                current_position += 5
+                current_position += len(matches.group())
                 continue
 
             for i in range(min(len(text), self.max_text_length), 0, -1):
