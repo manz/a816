18c74a58d6933bf1862f9a6bb290b5ae29c6da34
Emmanuel Peralta <manz@ringum.net>
2025-01-01 18:45:25 +0100

    Removes as exception handling in codegen.

--- a816/parse/codegen.py
+++ a816/parse/codegen.py
@@ -345,7 +345,7 @@ def generate_if(
 
     try:
         condition = eval_expression(node.expression, resolver)
-    except (KeyError, SymbolNotDefined) as e:
+    except (KeyError, SymbolNotDefined):
         condition = False
     if condition:
         code += _code_gen(if_branch_true.body, resolver, macro_definitions)
